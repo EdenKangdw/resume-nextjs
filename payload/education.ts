6bdd5f185397fc1c6025555c1230e85b90926a6c
Eden Kang <edenkang5517@gmail.com>
2025-02-27 00:53:31 +0900

    update : 대학원

--- payload/education.ts
+++ payload/education.ts
@@ -5,20 +5,26 @@ const education: IEducation.Payload = {
 
   list: [
     {
+      title: '서울과학종합대학원대학교(ASSIST)',
+      subTitle: 'AI빅데이터 학과 석사과정 재학중',
+      startedAt: '2024-09',
+      endedAt: '2025-08',
+    },
+    {
       title: '딥노이드 AI 머신러닝 교육 6주',
       subTitle: '수료',
       startedAt: '2021-11',
       endedAt: '2021-12',
     },
     {
-      title: '독학사(컴퓨터과학) - 학사',
-      subTitle: '취득',
+      title: '독학사(컴퓨터과학)',
+      subTitle: '학사',
       startedAt: '2021-01',
       endedAt: '2022-02',
     },
     {
       title: 'JAVA 프로그래밍 교육',
-      subTitle: '수료',
+      subTitle: '국비지원 교육 수료',
       startedAt: '2017-09',
       endedAt: '2018-03',
     },
